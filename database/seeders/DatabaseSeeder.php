0601663d1a7cae9bc8d0b401106f966f4f913248
AngelMezaSchool <al03018750@tecmilenio.mx>
2024-03-11 16:05:04 -0600

    tarea-6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Curso;
-
+use Database\Seeders\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -21,5 +21,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(CursoSeeder::class);
+
+        $this->call(UserSeeder::class);
+
+
     }
 }
